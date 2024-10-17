add9bb4fd08cbfd75917693d8ab78b8ca5b49c4c
Robert Levine <leviner@uw.edu>
2024-10-17 14:41:16 -0700

    add try/sleep for initial setup

--- Echologger/microcontroller/ECTLogger/code.py
+++ Echologger/microcontroller/ECTLogger/code.py
@@ -1,9 +1,14 @@
 from logger import rp2040_logger
 from params import params
+import alarm
 import time
 
 # Initiatie the logger
-rp = rp2040_logger(params)
+try:
+    rp = rp2040_logger(params)
+except: # If we fail to start up, just go back to sleep using the 'no clock' waiting period using the internal time alarm
+    time_alarm = alarm.time.TimeAlarm(monotonic_time = time.monotonic()+(params.wakeup_interval*60))
+    alarm.exit_and_deep_sleep_until_alarms(time_alarm)
 
 # If the microcontroller setup correctly, try to wake the ect
 if rp.wakeup_flag == 0:
@@ -29,14 +34,10 @@ if rp.wakeup_flag ==1:
         rp.pixel.brightness = 0.5
         rp.pixel[0] = (0,10,0)
         time.sleep(0.5)
-        rp.pixel.brightness = 0
-        time.sleep(0.5)
 else:
     for i in range(2):
         rp.pixel.brightness = 0.5
         rp.pixel[0] = (10,0,0)
-        time.sleep(0.5)
-        rp.pixel.brightness = 0
         time.sleep(0.5)
     
 # Close log, set timer and go to sleep
